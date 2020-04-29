3eda5fdb47babdae7de727a8b7e246d02bc7f574
Nicolas Kornberg <nicokorn96@gmail.com>
2020-04-29 17:03:48 -0300

    added cors

--- server.js
+++ server.js
@@ -11,10 +11,15 @@ var taskController = require('./controllers/taskController');
 
 var app = express();
 
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+};
 
 //app.use
 app.use(bodyParser.json());
 app.use('/tasks',taskController)
+app.use(cors(corsOptions)); 
 
 
 app.listen(3000, () => console.log('Server running on port 3000'))
